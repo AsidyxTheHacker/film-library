66168d6d7775b2c8bb3d99182ef724c9f2019a9b
Adam <adammiller.2000nj@gmail.com>
2023-05-28 15:34:43 -0400

    button functionality with adding and removing film items & modal

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let modal = document.querySelector(".modal-content");
 let myLibrary = [];
 
 function Film() {
@@ -5,5 +6,19 @@ function Film() {
 }
 
 function addFilm() {
-    
+    let filmContainer = document.querySelector(".film-display");
+    let slot = document.createElement('div');
+    let s = 4;
+    filmContainer.appendChild(slot).className = "film-item"
+    slot.id = `film-${++s}`;
+}
+
+function revealModal() {
+    modal.classList.remove("hidden");
+    modal.classList.add("active");
+}
+
+function hideModal() {
+    modal.classList.remove("active");
+    modal.classList.add("hidden");
 }
