a42b1d7dca66a5b7030d4b580cc4a1490fa0a113
Adam <adammiller.2000nj@gmail.com>
2023-05-31 18:12:21 -0400

    made the addFilm() function applicable

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
-let myLibrary = [];
-
-let modalBackground = document.querySelector(".modal-faded-background")
-let filmContainer = document.querySelector(".film-display");
-const modal = document.querySelector(".modal-content");
 const form = document.getElementById('addFilmForm');
-const title = document.getElementById('title');
-const director = document.getElementById('director');
-const poster = document.getElementById('poster');
 const saveBtn = document.getElementById('saveBtn');
 const exitBtn = document.getElementById('exitBtn');
+const filmContainer = document.querySelector(".film-display");
+const modalBackground = document.querySelector(".modal-faded-background")
+const modal = document.querySelector(".modal-content");
 
 class Film { // Film constructor
-    constructor(title, director, poster = "imgs/missing-poster.png") {
+    constructor(id, title, director, poster) {
+        this.id = id;
         this.title = title;
         this.director = director;
         this.poster = poster;
     }
 }
 
-function addFilm() { // Add film to catalog
-    const newFilm = new Film(title, director, poster)
-
-    createFilm();
-}
+let myLibrary = [];
 
 function createFilm() { // Creates the film object
-    let deleteBtn = document.createElement('img');
-    let filmTitle = document.createElement('p');
-    let slot = document.createElement('div');
-    deleteBtn.src = "imgs/trash.svg";
-    filmContainer.appendChild(slot).className = "film-item";
-    slot.appendChild(deleteBtn).id = "delete-film";
-    slot.appendChild(filmTitle).className = "film-title";
+    myLibrary.forEach(Film => {
+        let filmSlot = document.createElement('div');
+        let deleteBtn = document.createElement('img');
+        let filmPoster = document.createElement('img');
+        let filmTitle = document.createElement('p');
+        let filmDirector = document.createElement('p');
+        filmSlot.id = Film.id;
+        filmTitle.textContent = Film.title;
+        filmDirector.textContent = Film.director;
+        deleteBtn.src = "imgs/trash.svg";
+        filmPoster.src = Film.poster;
+        filmContainer.appendChild(filmSlot).className = "film-item";
+        filmSlot.appendChild(filmPoster).className = "movie-poster";
+        filmSlot.appendChild(deleteBtn).id = "delete-film";
+        filmSlot.appendChild(filmTitle).className = "film-title";
+    })
+}
+
+function addFilm() { // Add film to catalog
+    let id = Date.now();
+    let title = document.getElementById('title').value;
+    let director = document.getElementById('director').value;
+    let poster = document.getElementById('poster').value;
+    let newFilm = new Film(id, title, director, poster);
+    if (title === title) {
+        myLibrary.pop(newFilm);
+    }
+    myLibrary.push(newFilm);
+    createFilm();
 }
 
 function clearForm() {
